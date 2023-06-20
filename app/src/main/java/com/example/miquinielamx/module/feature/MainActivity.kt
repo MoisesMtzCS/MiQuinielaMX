c3616687339d762a3910101ba980c736c2f80737
Moises Martinez <moisesmtzcs@MacBook-Pro-de-Moises.local>
2023-06-20 16:32:48 -0600

    Added: setup crashlytics

--- app/src/main/java/com/example/miquinielamx/module/feature/MainActivity.kt
+++ app/src/main/java/com/example/miquinielamx/module/feature/MainActivity.kt
@@ -3,10 +3,21 @@ package com.example.miquinielamx.module.feature
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.miquinielamx.R
+import com.example.miquinielamx.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /* Android resources */
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    /*****************************************************************************************
+     * LIFECYCLE
+     ****************************************************************************************/
+
+    /** Called at create the screen. */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
 }
